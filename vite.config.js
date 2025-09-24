754b65bac402dbc066b89f40d3d80db661a1b1a0
Joao vitor <jvzinn2074@gmail.com>
2025-09-23 22:07:33 -0300

    comitando

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [/Portfolio/],
+  base: '/Portfolio/', // <-- caminho correto para GitHub Pages
+  plugins: [react()],
 })
